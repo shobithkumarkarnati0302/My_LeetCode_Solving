d60b87b579330742ef721e0c51ff54973a874d85
Karnati Shobith Kumar <161016030+shobithkumarkarnati0302@users.noreply.github.com>
2025-05-13 21:03:27 +0530

    Time: 11 ms (39.76%), Space: 12.4 MB (86.27%) - LeetHub

---
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def decrypt(self, code, k):
+        n = len(code)
+        ans = [0] * n
+        if k == 0:
+            return ans
+        for i in range(n):
+            if k > 0:
+                for j in range(i + 1, i + k + 1):
+                    ans[i] += code[j % n]
+            else:
+                for j in range(i + k, i):
+                    ans[i] += code[(j + n) % n]
+        return ans
+        
