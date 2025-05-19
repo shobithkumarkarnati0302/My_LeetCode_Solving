68261c81aa6e0ecf99a1ba2968d419f608950540
Karnati Shobith Kumar <161016030+shobithkumarkarnati0302@users.noreply.github.com>
2025-05-19 11:02:40 +0530

    Time: 0 ms (100%), Space: 12.4 MB (89.47%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.py
+++ 0861-flipping-an-image/0861-flipping-an-image.py
@@ -8,15 +8,16 @@ class Solution(object):
             end = col-1
 
             while start <= end:
-                image[i][start],image[i][end] = image[i][end],image[i][start]
+                image[i][start],image[i][end] = image[i][end]^1,image[i][start]^1
                 start += 1
                 end -= 1
             
-        for i in range(row):
-            for j in range(col):
-                if image[i][j] == 0:
-                    image[i][j] = 1
-                else:
-                    image[i][j] = 0
+        # for i in range(row):
+        #     for j in range(col):
+        #         if image[i][j] == 0:
+        #             image[i][j] = 1
+        #         else:
+        #             image[i][j] = 0
+
         return image
         
