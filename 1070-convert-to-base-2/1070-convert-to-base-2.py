29c896314b33626adda1cc7d4825dc63931c3b62
Karnati Shobith Kumar <161016030+shobithkumarkarnati0302@users.noreply.github.com>
2025-06-01 00:33:21 +0530

    Time: 0 ms (100%), Space: 12.4 MB (46.67%) - LeetHub

---
+++ 1070-convert-to-base-2/1070-convert-to-base-2.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def baseNeg2(self, n):
+        k = 1
+        ans = []
+        while n:
+            if n % 2:
+                ans.append('1')
+                n -= k
+            else:
+                ans.append('0')
+            n //= 2
+            k *= -1
+        return ''.join(ans[::-1]) or '0'
+        
