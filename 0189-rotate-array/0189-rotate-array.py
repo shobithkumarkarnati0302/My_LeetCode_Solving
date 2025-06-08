05bb64cd00b0a76172488a557b77f1b936d464bd
Karnati Shobith Kumar <161016030+shobithkumarkarnati0302@users.noreply.github.com>
2025-06-08 21:43:48 +0530

    Time: 1 ms (78.5%), Space: 22.6 MB (39.58%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -2,3 +2,8 @@ class Solution(object):
     def rotate(self, nums, k):
         k %= len(nums)
         nums[:] = nums[-k:] + nums[:-k]
+
+        '''
+        here nums[:] is used because we need to sollve it in O(1) space
+        and nums[:] modifies the 
+        '''
