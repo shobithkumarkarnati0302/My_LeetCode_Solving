39c9132e2139bf5d338ad1e33087f7269db50ea3
Karnati Shobith Kumar <161016030+shobithkumarkarnati0302@users.noreply.github.com>
2025-06-11 16:38:31 +0530

    Time: 4 ms (12.53%), Space: 12.3 MB (81.21%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def checkValidString(self, s):
+        x = 0
+        for c in s:
+            if c in '(*':
+                x += 1
+            elif x:
+                x -= 1
+            else:
+                return False
+        x = 0
+        for c in s[::-1]:
+            if c in '*)':
+                x += 1
+            elif x:
+                x -= 1
+            else:
+                return False
+        return True
