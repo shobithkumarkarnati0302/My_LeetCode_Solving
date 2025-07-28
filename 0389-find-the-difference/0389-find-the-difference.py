95aebe180e2ab24cb20ece3c26970a3f5f842058
Karnati Shobith Kumar <161016030+shobithkumarkarnati0302@users.noreply.github.com>
2025-07-28 15:24:38 +0530

    Time: 3 ms (68.95%), Space: 12.5 MB (23.41%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,10 +1,20 @@
 class Solution(object):
     def findTheDifference(self, s, t):
-        s_list = list(s)
-        t_list = list(t)
+        # s_list = list(s)
+        # t_list = list(t)
         
-        for i in s_list:
-            if i in t_list:
-                t_list.remove(i)
+        # for i in s_list:
+        #     if i in t_list:
+        #         t_list.remove(i)
         
-        return "".join(t_list)
+        # return "".join(t_list)
+
+        s_ans = 0
+        t_ans = 0
+
+        for i in s:
+            s_ans += ord(i)
+        for j in t:
+            t_ans += ord(j)
+
+        return chr(t_ans-s_ans)
